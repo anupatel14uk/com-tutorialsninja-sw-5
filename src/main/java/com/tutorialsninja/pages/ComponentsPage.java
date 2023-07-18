cd3dd12a1b448e7d2a705617d363d4bba989d311
anupatel14uk <anupatel14uk@gmail.com>
2023-07-18 21:32:57 +0100

    Week5 SeleniumHw

---
+++ src/main/java/com/tutorialsninja/pages/ComponentsPage.java
@@ -0,0 +1,18 @@
+package com.tutorialsninja.pages;
+
+import com.tutorialsninja.utilities.Utility;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Reporter;
+
+public class ComponentsPage extends Utility {
+    @CacheLookup
+    @FindBy(xpath = "//h2[contains(text(),'Components')]")
+    WebElement componentsText;
+
+    public String getComponentsText() {
+        Reporter.log("Get component Text  " + componentsText.toString());
+        return getTextFromElement(componentsText);
+    }
+}
